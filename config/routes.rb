f3c1beb8950a2eee53b6d78c89a18b235bef11c5
Simon Huang <firebfm@gmail.com>
2021-02-04 21:51:30 +0000

    generate model user, rake db, add routes, generate controller users

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   namespace :admin do
     root to: 'dashboard#show'
